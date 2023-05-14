021e610b903839ad29ba70da71f18c39e2dc950c
Grenate22 <104887014+Grenate22@users.noreply.github.com>
2023-05-14 12:47:56 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 #SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
